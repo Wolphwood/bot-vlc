87b9d771d0157dc89ded2075ef21dc8b08cbfe94
Wolphwood <wolphwoodthegamer@gmail.com>
2026-05-03 19:34:01 +0200

    🐦‍⬛ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⡀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣰⣜⣽⣦⡄⣎ ⠀⡀⣦⡀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢦⣻⣿⣿⣿⣿⡇ ⡀⠹⡪⣿⣾⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠐⢦⣝⣿⣿⣿⣿⠁ ⠘⠷⣾⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⠏⠀ ⠠⢥⣴⣾⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⢀⣲⣿⣿⣿⣿⠟⠀⠀ ⠀⠀⢂⣩⣵⢿⣿⣿⣿⣿⣿⣿⣄⢠⣾⣿⣿⣿⣯⣥⡀⠀⠀ ⠀⠀⠀⠠⠤⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀ ⠀⠀⠀⠀⠀⠺⢯⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠱⠑⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⡿⠛⠁⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⢀⣴⡿⣿⣿⡿⠻⣯⠀⠧⢀⣀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠉⠀⠈⠈⠀⠀⠀⣵⢦⠈⠿⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠀⠀⠀⠀⠀⠀⠀⠀

---
+++ assets/langs/fr/commands/help.js
@@ -0,0 +1,24 @@
+export default {
+  "commandinfo.help.description":"Cette commande permet de faire du couscous.",
+  "commandinfo.help.syntax": "Help <commande> [sous-commande]",
+  "commandinfo.help.option.search.description": "Filtre de recherche.",
+
+  "command.help.embed.title":"Voici la liste des commandes.",
+  "command.help.embed.description":"Les commandes marquées d'une étoile (\\*) possèdent des sous commandes.\nLes `<>` désignent un ou plusieurs argument(s) obligatoires, `[]` désignent un ou plusieurs argument(s) facultatifs et `|` signifie `ou`, il ne **faut pas** les mettres.\n\u200b",
+  "command.help.embed.footer":"%0a v%1a | Vitesse de navigation : %2a | Page %3a/%4a",
+  "command.help.embed.closereason.user":"L'utilisateur à fermé ce menu.",
+  "command.help.embed.closereason.idle":"Ce menu à été fermé pour inactivité.",
+  
+  "command.help.modal.goto.title": "Naviguation rapide.",
+  "command.help.modal.goto.label": "Numéro de page.",
+  "command.help.modal.goto.placeholder": "Chiffre entre %0a et %1a (compris)",
+  "command.help.modal.new_search.title": "Nouvelle recherche",
+  "command.help.modal.new_search.label": "Entrez la nouvelle recherche.",
+  "command.help.modal.new_search.placeholder": "Filter value",
+  
+  
+  "command.help.button.close": "Fermer",
+  "command.help.button.new_search": "Nouvelle recherche",
+  
+  "command.help.error.notfound": "<@%0a>, Aucune commande trouvée pour le filtre renseigné.",
+}
